82677e9d61004c3b63fa4fbcfca8a47b1646b583
Johannes Simon <johannes.simon@mail.de>
2014-10-27 11:27:11 +0100

    Aggregate features per word similarity

--- src/main/scala/WordSim.scala
+++ src/main/scala/WordSim.scala
@@ -1,29 +1,30 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 
 object WordSim {
     def log2(n:Double): Double = {
         math.log(n) / math.log(2)
     }
-    
+
     def ll(n:Long, wc:Long, fc:Long, bc:Long): Double = {
         val wcL = log2(wc)
         val fcL = log2(fc)
         val bcL = log2(bc)
         val epsilon = 0.000001
         val res = 2*(n*log2(n)
-                            -wc*wcL
-                            -fc*fcL
-                            +bc*bcL
-                            +(n-wc-fc+bc)*log2(n-wc-fc+bc+epsilon)
-                            +(wc-bc)*log2(wc-bc+epsilon)
-                            +(fc-bc)*log2(fc-bc+epsilon)
-                            -(n-wc)*log2(n-wc+epsilon)
-                            -(n-fc)*log2(n-fc+epsilon) )
+            -wc*wcL
+            -fc*fcL
+            +bc*bcL
+            +(n-wc-fc+bc)*log2(n-wc-fc+bc+epsilon)
+            +(wc-bc)*log2(wc-bc+epsilon)
+            +(fc-bc)*log2(fc-bc+epsilon)
+            -(n-wc)*log2(n-wc+epsilon)
+            -(n-fc)*log2(n-fc+epsilon) )
         if ((n*bc)<(wc*fc)) -res.toDouble else res.toDouble
     }
-    
+
     def lmi(n:Long, wc:Long, fc:Long, bc:Long): Double = {
         bc*math.log( (n*bc).toFloat/(wc*fc) )/math.log(2)
     }
@@ -45,7 +46,7 @@ object WordSim {
         val wordFeaturesOccurrences = file
             .map(line => line.split("\t"))
             .map({case Array(word, feature, dataset, wordPos, featurePos) => (word, feature, dataset, wordPos, featurePos)
-                  case _ => ("BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE")})
+        case _ => ("BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE", "BROKEN_LINE")})
         wordFeaturesOccurrences.cache()
 
         val wordFeatureCounts = wordFeaturesOccurrences
@@ -55,6 +56,10 @@ object WordSim {
             .reduceByKey((v1, v2) => v1 + v2)
             .map({case ((word, feature), count) => (word, (feature, count))})
         wordFeatureCounts.cache()
+
+        val n = wordFeatureCounts
+            .map({case (word, (feature, wfc)) => (feature, (word, wfc))})
+            .aggregate(0L)(_ + _._2._2.toLong, _ + _) // we need Long because n might exceed the max. Int value
 
         val wordCounts = wordFeaturesOccurrences
             .map({case (word, feature, dataset, wordPos, featurePos) => ((word, dataset, wordPos), 1)})
@@ -82,12 +87,6 @@ object WordSim {
             .filter({case (word, (feature, wfc)) => wfc >= param_t})
         wordFeatureCountsFiltered.cache()
 
-        val n = wordFeatureCountsFiltered
-            .map({case (word, (feature, wfc)) => (feature, (word, wfc))})
-            //.join(wordsPerFeature) // filter by using a join
-            //.map({case (feature, ((word, wfc), fwc)) => (word, (feature, wfc))})
-            .aggregate(0L)(_ + _._2._2.toLong, _ + _) // we need Long because n might exceed the max. Int value
-
         val featuresPerWordWithScore = wordFeatureCountsFiltered
             .join(wordCounts)
             .map({case (word, ((feature, wfc), wc)) => (feature, (word, wfc, wc))})
@@ -98,18 +97,31 @@ object WordSim {
             // (word, [(feature, score), (feature, score), ...])
             .mapValues(featureScores => featureScores.toArray.sortWith({case ((_, s1), (_, s2)) => s1 > s2}).take(param_p)) // sort by value desc
 
+        val featuresPerWord:RDD[(String, Array[String])] = featuresPerWordWithScore
+            .map({case (word, featureScores) => (word, featureScores.map({case (feature, score) => feature}))})
+
         val wordsPerFeatureWithScore = featuresPerWordWithScore
             .flatMap({case (word, featureScores) => for(featureScore <- featureScores) yield (featureScore._1, (word, 1))})
             .groupByKey()
         wordsPerFeatureWithScore.cache()
 
-        val wordSims = wordsPerFeatureWithScore
+        val wordSims:RDD[(String, (String, Int))] = wordsPerFeatureWithScore
             .flatMap({case (feature, wordScores) => for((word1, score1) <- wordScores; (word2, score2) <- wordScores) yield ((word1, word2), score2)})
             .reduceByKey((score1, score2) => score1 + score2)
             .map({case ((word1, word2), score) => (word1, (word2, score))})
             .groupByKey()
             .mapValues(simWords => simWords.toArray.sortWith({case ((_, s1), (_, s2)) => s1 > s2}).take(param_l))
             .flatMap({case (word, simWords) => for(simWord <- simWords) yield (word, simWord)})
+
+        val wordSimsWithFeatures:RDD[(String, (String, Int, Set[String]))] = wordSims
+            .join(featuresPerWord)
+            .map({case (word, ((simWord, score), featureList1)) => (simWord, (word, score, featureList1))})
+            .join(featuresPerWord)
+            .map({case (word, ((simWord, score, featureList1), featureList2)) => (word, (simWord, score, featureList1.toSet.intersect(featureList2.toSet)))})
+
+        wordSimsWithFeatures
+            .map({case (word1, (word2, score, featureSet)) => word1 + "\t" + word2 + "\t" + score + "\t" + featureSet.mkString("  ")})
+            .saveAsTextFile(dir + "__LMI_SimWithFeatures")
 
         if (param_debug) {
             wordCounts
