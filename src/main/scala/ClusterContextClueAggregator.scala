71b35b6b663a318d6c9bc29180a228f6551517dc
Johannes Simon <johannes.simon@mail.de>
2014-11-11 17:07:41 +0100

    - add coverage threashold

--- src/main/scala/ClusterContextClueAggregator.scala
+++ src/main/scala/ClusterContextClueAggregator.scala
@@ -6,11 +6,12 @@ import org.apache.spark.rdd._
 object ClusterContextClueAggregator {
     def main(args: Array[String]) {
         if (args.size < 3) {
-            println("Usage: ClusterContextClueAggregator cluster-file feature-file output [s]")
+            println("Usage: ClusterContextClueAggregator cluster-file feature-file output [min. probability] [min. coverage]")
             return
         }
 
         val param_s = if (args.length > 3) args(3).toDouble else 0.0
+        val param_p = if (args.length > 4) args(4).toDouble else 0.0
 
         val conf = new SparkConf().setAppName("ClusterContextClueAggregator")
         val sc = new SparkContext(conf)
@@ -26,12 +27,12 @@ object ClusterContextClueAggregator {
 
         val wordFeatures = featureFile
             .map(line => line.split("\t"))
-            .map(cols => (cols(0), (cols(1), cols(4).toFloat / cols(3).toFloat))) // prob = wfc / fc
-            .filter({case (word, (feature, prob)) => prob > param_s})
+            .map(cols => (cols(0), (cols(1), cols(4).toFloat / cols(3).toFloat, cols(4).toFloat / cols(2).toFloat))) // prob = wfc / fc, coverage = wfc / wc
+            .filter({case (word, (feature, prob, coverage)) => prob >= param_s && coverage >= param_p})
 
         clusterWords
             .join(wordFeatures)
-            .map({case (simWord, ((word, sense), (feature, score))) => ((word, sense, feature), (score, 1.0f))})
+            .map({case (simWord, ((word, sense), (feature, prob, coverage))) => ((word, sense, feature), (prob, 1.0f))})
             .reduceByKey({case ((v1, n1), (v2, n2)) => (v1 + v2, n1 + n2)})
             .map({case ((word, sense, feature), (score, n)) => ((word, sense), (feature, score / n))})
             .groupByKey()
