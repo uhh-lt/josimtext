c39df2e6debd3c696e5e663c60dd0f6ac28298a3
Johannes Simon <johannes.simon@mail.de>
2014-11-26 19:35:55 +0100

    - bugfix

--- src/main/scala/WordSimFromCounts.scala
+++ src/main/scala/WordSimFromCounts.scala
@@ -19,9 +19,9 @@ object WordSimFromCounts {
         val param_t_w = if (args.size > 7) args(7).toInt else 2
         val param_t_f = if (args.size > 8) args(8).toInt else 2
         val param_sig = if (args.size > 9) args(9) else "LMI"
-        val param_p1 = if (args.size > 10) args(10).toInt else 1000
-        val param_p2 = if (args.size > 11) args(11).toInt else 1000
-        val param_r = if (args.size > 12) args(12).toInt else 1000
+        val param_r = if (args.size > 10) args(10).toInt else 3
+        val param_p1 = if (args.size > 11) args(11).toInt else 1000
+        val param_p2 = if (args.size > 12) args(12).toInt else 1000
         val param_l = if (args.size > 13) args(13).toInt else 200
 
         def sig(_n:Long, wc:Long, fc:Long, bc:Long) =
