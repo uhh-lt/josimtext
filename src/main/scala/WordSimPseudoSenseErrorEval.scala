9235e4f80e727ba0573138368bf0954af22b4c50
Johannes Simon <johannes.simon@mail.de>
2015-01-09 11:38:55 +0100

    - enhance sim error evaluation

--- src/main/scala/WordSimPseudoSenseErrorEval.scala
+++ src/main/scala/WordSimPseudoSenseErrorEval.scala
@@ -3,10 +3,13 @@ import org.apache.spark.SparkContext._
 
 object WordSimPseudoSenseErrorEval {
 
-    def computeRelativeError(word1:String, word2:String, scores:Array[Float]):Float = {
+    def computeRelativeError(word1:String, word2:String, scores:Array[Float]):(Float, Float) = {
         if (scores.length == 2) {
-            2*math.abs(scores(0) - scores(1)) / (scores(0) + scores(1))
-        } else 1 // if the other sense does not appear at all that's equivalent to sim = 0
+            (
+                math.abs(scores(0) - scores(1)) / math.max(scores(0), scores(1)),
+                math.abs(scores(0) - scores(1))
+            )
+        } else (1,1) // if the other sense does not appear at all that's equivalent to sim = 0
     }
 
     def main(args: Array[String]) {
@@ -21,14 +24,24 @@ object WordSimPseudoSenseErrorEval {
         val conf = new SparkConf().setAppName("WordSim")
         val sc = new SparkContext(conf)
 
-        val res:(Float, Int) = sc.textFile(param_dataset)
+        val res = sc.textFile(param_dataset)
             .map(line => line.split("\t"))
             .filter(array => array.length == 4) // filter erroneous lines
             .map({case Array(word1, word2, score, features) => ((word1, word2.replaceAll("\\$\\$.*", "")), score.toFloat)})
             .groupByKey()
-            .map({case ((word1, word2), scores) => (computeRelativeError(word1, word2, scores.toArray), 1)})
-            .reduce({case ((score1, aggr1), (score2, aggr2)) => (score1+score2, aggr1+aggr2)})
+            .map({case ((word1, word2), scores) => (word1.replaceAll("\\$\\$.*", ""), (computeRelativeError(word1, word2, scores.toArray), 1))})
+            .reduceByKey({case (((score11, score12), aggr1), ((score21, score22), aggr2)) => ((score11+score21,score12+score22), aggr1+aggr2)})
+            .map({case (word, ((relErrorSum, absErrorSum), aggr)) => (word, (relErrorSum / aggr, absErrorSum / aggr))})
 
-        println("Result: " + res._1 / res._2)
+        res.map({case (word, (relErrorSum, absErrorSum)) => word + "\t" + relErrorSum + "\t" + absErrorSum})
+           .saveAsTextFile(outDir + "AvgRelErrorPerWord")
+
+        res.map({case (word, (avgRelError, avgAbsError)) => ("TOTAL", ((avgRelError, avgAbsError), 1))})
+            .reduceByKey({case (((score11, score12), aggr1), ((score21, score22), aggr2)) => ((score11+score21,score12+score22), aggr1+aggr2)})
+            .map({case (word, ((avgRelErrorSum, avgAbsErrorSum), aggr)) => (word, (avgRelErrorSum / aggr, avgAbsErrorSum / aggr))})
+            .map({case (word, (relErrorSum, absErrorSum)) => word + "\t" + relErrorSum + "\t" + absErrorSum})
+            .saveAsTextFile(outDir + "AvgRelErrorTotal")
+
+
     }
 }
