b88ecc5be6aad133eb66d74429e0e5ecbc9e4685
Johannes Simon <johannes.simon@mail.de>
2015-02-05 15:20:12 +0100

    - new stuff

---
+++ src/main/scala/ClusterForWSDOptimizer.scala
@@ -0,0 +1,113 @@
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object ClusterForWSDOptimizer {
+
+    def computeFeatureValues(word:String, featureValues:String, clusterSize:Int): (String, Double) = {
+        val featureArr = Util.splitLastN(featureValues, ':', 2)
+        val feature = featureArr(0)
+        if (featureArr.length != 2)
+            return null
+        val prob     = featureArr(1).toDouble // P(c|s_k*)
+        if (feature.equals(word))
+            return null
+        (feature, prob)
+    }
+
+    def computeFeatureProbs(word:String, featuresWithValues:Array[String], clusterSize:Int, senseCount:Double, p:Int): Map[String, Double] = {
+        val res = featuresWithValues
+            .map(featureValues => computeFeatureValues(word, featureValues, clusterSize))
+            .filter(_ != null)
+            //.map({case (feature, prob) => (feature, prob * prob * senseCount / featureCounts(feature))})
+            .sortBy(_._2)
+            .reverse
+            .take(p)
+        res.toMap
+    }
+
+    def calcScore(cluster1:(Int, (Double, Seq[String], Map[String, Double])), cluster2:(Int, (Double, Seq[String], Map[String, Double]))):Double = {
+        val features1 = cluster1._2._3
+        val features2 = cluster2._2._3
+        /*val featureUnion = features1.keySet.union(features2.keySet)
+        var totalDiff = 0.0
+        var totalProb1 = math.log(cluster1._2._1)
+        var totalProb2 = math.log(cluster2._2._1)
+        for (feature <- featureUnion) {
+            val p1 = if (features1 contains feature) features1(feature) else 0.0
+            val p2 = if (features2 contains feature) features2(feature) else 0.0
+            val diff = math.abs(p1 - p2)
+            val relDiff = diff / math.max(p1,p2)
+            totalDiff += relDiff
+            totalProb1 = math.log(p1 + 0.00001)
+            totalProb2 = math.log(p2 + 0.00001)
+        }
+        val avgDiff = totalDiff / featureUnion.size
+        val probDiff = math.abs(totalProb1 - totalProb2)*/
+        val numFeatures = math.max(features1.size, features2.size)
+        val featureIntersection = features1.keySet.intersect(features2.keySet).size
+        featureIntersection / numFeatures.toDouble
+    }
+
+    def pruneClusters(clusters:Seq[(Int, (Double, Seq[String], Map[String, Double]))], simThreshold:Double):Seq[(Int, (Double, Seq[String], Map[String, Double]))] = {
+        var clustersPruned:Seq[(Int, (Double, Seq[String], Map[String, Double]))] = List()
+        for (i <- 0 to clusters.size - 1) {
+            val cluster1 = clusters(i)
+            var dropCluster = false
+            for (j <- 0 to clusters.size - 1) {
+                if (i < j) {
+                    val cluster2 = clusters(j)
+                    val sim = calcScore(cluster1, cluster2)
+                    if (sim >= simThreshold) {
+                        dropCluster = true
+                    }
+                }
+            }
+            if (!dropCluster) {
+                clustersPruned = clustersPruned :+ cluster1
+            }
+        }
+        clustersPruned
+    }
+
+    def main(args: Array[String]) {
+        if (args.size < 1) {
+            println("Usage: ClusterForWSDOptimizer cluster-file-with-clues")
+            return
+        }
+
+        val conf = new SparkConf().setAppName("ClusterForWSDOptimizer")
+        val sc = new SparkContext(conf)
+
+        val clusterFile = sc.textFile(args(0))
+        val p = args(1).toInt
+        val simThreshold = args(2).toDouble
+        val outputFile = args(0) + "__Optimized"
+
+        /*val featureCounts = featureFile
+            .map(line => line.split("\t"))
+            .map({case Array(word, count) => (word, count.toInt)})
+            .filter({case (word, count) => count >= 2})
+            .collect()
+            .toMap*/
+
+        // (lemma, (sense -> (feature -> prob)))
+        val clustersWithClues = clusterFile
+            .map(line => line.split("\t"))
+            .map({case Array(lemma, sense, senseLabel, senseCount, simWords, featuresWithValues) => (lemma, sense.toInt, senseCount.toDouble, simWords.split("  "), featuresWithValues.split("  "))})
+            .map({case (lemma, sense, senseCount, simWords, featuresWithValues) => (lemma, (sense, (senseCount, simWords.toSeq, computeFeatureProbs(lemma, featuresWithValues, simWords.size, senseCount, p))))})
+            .groupByKey()
+            .mapValues(clusters => pruneClusters(clusters.toSeq, simThreshold))
+            .flatMap({case (lemma, clusters) => for (cluster <- clusters) yield (lemma, cluster)})
+
+        clustersWithClues
+            .map({case (word, (sense, (senseCount, simWordsWithSim, senseFeatureProbs))) =>
+                word + "\t" +
+                sense + "\t" +
+                senseCount + "\t" +
+                simWordsWithSim.mkString("  ") + "\t" +
+                senseFeatureProbs.map(tuple => tuple._1 + ":" + tuple._2).mkString("  ")})
+            .saveAsTextFile(outputFile)
+    }
+}
