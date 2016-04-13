66b9b7a279214b9761161656d25717359a187bce
Alexander Panchenko <panchenko.alexander@gmail.com>
2016-04-13 17:42:09 +0200

    meta-combination cli interface

--- scripts/postprocessing/combine.py
+++ scripts/postprocessing/combine.py
@@ -1,37 +1,49 @@
 from combine_lib import combine_majority, combine_ranks, combine_weighted_average
 from interpret_lib import load_lexsamples, predict_lexsample
 from os.path import join
+import argparse
 
 
-def combine(lexsample_fpaths):
+def combine(lexsample_fpaths, output_dir):
     lexsamples = load_lexsamples(lexsample_fpaths, header=None)
     for fpath in lexsamples:
         predict_lexsample(lexsamples[fpath], join(output_dir, fpath + "-re2.csv"))
     lexsamples = load_lexsamples([fpath + "-re2.csv" for fpath in lexsample_fpaths], header=None)
 
     cmb_name = "+".join(x.split("-")[0] for x in lexsamples)
-    lexsample_cmb = combine_majority(lexsamples, join(output_dir, "cmb-majority-%s.csv" % cmb_name ))
-    lexsample_cmb = combine_weighted_average(lexsamples, join(output_dir, "cmb-weighted-avg-reject50-%s.csv" % cmb_name), reject_more_than_50=True)
-    lexsample_cmb = combine_weighted_average(lexsamples, join(output_dir, "cmb-weighted-avg-%s.csv" % cmb_name), reject_more_than_50=False)
-    lexsample_cmb = combine_ranks(lexsamples, join(output_dir, "cmb-ranks-%s.csv" % cmb_name))
-    lexsample_cmb = combine_ranks(lexsamples, join(output_dir, "cmb-ranks-ignore-reject-%s.csv" % cmb_name), ignore_rejected=True)
+    combine_majority(lexsamples, join(output_dir, "cmb-majority-%s.csv" % cmb_name ))
+    combine_weighted_average(lexsamples, join(output_dir, "cmb-weighted-avg-reject50-%s.csv" % cmb_name), reject_more_than_50=True)
+    combine_weighted_average(lexsamples, join(output_dir, "cmb-weighted-avg-%s.csv" % cmb_name), reject_more_than_50=False)
+    combine_ranks(lexsamples, join(output_dir, "cmb-ranks-%s.csv" % cmb_name))
+    combine_ranks(lexsamples, join(output_dir, "cmb-ranks-ignore-reject-%s.csv" % cmb_name), ignore_rejected=True)
 
+def main():
+    lexsample_description = 'Output of the WSD class: a lexical sample file with features and predictions (18 columns, no header)'
+    parser = argparse.ArgumentParser(description='Combine single classifiers in different ways using meta-combinations.')
+    parser.add_argument('output', help="Output directory with results.")
+    parser.add_argument('clusters', help=lexsample_description)
+    parser.add_argument('coocs', help=lexsample_description)
+    parser.add_argument('depstarget', help=lexsample_description)
+    parser.add_argument('depsall', help=lexsample_description)
+    parser.add_argument('trigramstarget', help=lexsample_description)
+    parser.add_argument('trigramsall', help=lexsample_description)
+    args = parser.parse_args()
+    print "Output:", args.output
+    print "Clusters:", args.clusters
+    print "Coocs:", args.coocs
+    print "Depstarget:", args.depstarget
+    print "Depsall:", args.depsall
+    print "Trigramstarget:", args.trigramstarget
+    print "Trigramsall:", args.trigramsall
 
+    combine([args.clusters], args.output)
+    # combine([args.clusters, args.coocs, args.depstarget, args.depsall, args.trigramstarget, args.trigramsall], args.output)
+    combine([args.clusters, args.coocs], args.output)
+    combine([args.coocs, args.depstarget], args.output)
+    combine([args.clusters, args.coocs, args.depstarget, args.trigramstarget], args.output)
+    combine([args.clusters, args.coocs, args.depsall, args.trigramsall], args.output)
+    combine([args.clusters, args.coocs, args.depstarget], args.output)
+    combine([args.clusters, args.coocs, args.depsall], args.output)
 
-clusters_fpath = "/Users/alex/Desktop/prj/test-res/results/Clusters-3"
-coocs_fpath = "/Users/alex/Desktop/prj/test-res/results/Depwords-4"
-depstarget_fpath = "/Users/alex/Desktop/prj/test-res/results/Depstarget-2"
-depsall_fpath = "/Users/alex/Desktop/prj/test-res/results/Depsall-4"
-trigramstarget_fpath = "/Users/alex/Desktop/prj/test-res/results/Trigramstarget-3"
-trigramsall_fpath = "/Users/alex/Desktop/prj/test-res/results/Trigramsall-4"
-
-output_dir = "/Users/alex/Desktop/prj/test-res/results/"
-
-# combine([clusters_fpath])
-# combine([clusters_fpath, coocs_fpath, depstarget_fpath, depsall_fpath, trigramstarget_fpath, trigramsall_fpath])
-# combine([clusters_fpath, coocs_fpath])
-# combine([coocs_fpath, depstarget_fpath])
-combine([clusters_fpath, coocs_fpath, depstarget_fpath, trigramstarget_fpath])
-combine([clusters_fpath, coocs_fpath, depsall_fpath, trigramsall_fpath])
-combine([clusters_fpath, coocs_fpath, depstarget_fpath])
-combine([clusters_fpath, coocs_fpath, depsall_fpath])
+if __name__ == '__main__':
+    main()
