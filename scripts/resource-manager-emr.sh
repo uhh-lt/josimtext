696e3c0de267a2e0c3909be3833f916a6dd3b69c
Alexander Panchenko <panchenko.alexander@gmail.com>
2017-06-01 11:15:54 +0200

    resource manager aws

---
+++ scripts/resource-manager-emr.sh
@@ -0,0 +1,4 @@
+master=ec2-52-50-233-45.eu-west-1.compute.amazonaws.com
+ssh -i ~/.ssh/ireland.pem -N -L 8088:$master:8088 hadoop@$master &
+ssh -i ~/.ssh/ireland.pem -N -L 20888:$master:20888 hadoop@$master &
+ssh -i ~/.ssh/ireland.pem -N -L 19888:$master:19888 hadoop@$master &
