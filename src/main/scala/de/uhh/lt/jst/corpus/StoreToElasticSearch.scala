5964b748b52611c5d961092709578965b90209a3
Alexander Panchenko <panchenko.alexander@gmail.com>
2017-11-29 19:35:22 +0100

    add es parameters

--- src/main/scala/de/uhh/lt/jst/corpus/StoreToElasticSearch.scala
+++ src/main/scala/de/uhh/lt/jst/corpus/StoreToElasticSearch.scala
@@ -13,7 +13,9 @@ import de.uhh.lt.conll._
 object StoreToElasticSearch extends Job {
   case class Config(inputDir: String = "",
                     outputIndex: String = "depcc/sentences",
-                    esNodeList: String = "localhost")
+                    esNodeList: String = "localhost",
+                    maxBatchMb: Int = 1,
+                    maxBatchDocs: Int = 1000)
   override type ConfigType = Config
   override val config = Config()
   override val description: String = "Index CoNLL file with ElasticSearch"
@@ -30,10 +32,17 @@ object StoreToElasticSearch extends Job {
     arg[String]("ES_NODES").action( (x, c) =>
       c.copy(esNodeList = x) ).required().
       text("List of ElasticSearch nodes where the output will be written (may be not exhaustive).")
+
+    arg[Int]("MAX_BATCH_MB").action( (x, c) =>
+      c.copy(maxBatchMb = x * 1000000) ).
+      text("Max. size of a batch in MB.")
+
+    arg[Int]("MAX_BATCH_DOCS").action( (x, c) =>
+      c.copy(maxBatchDocs = x) ).
+      text("Max. size of a batch in number of documents.")
   }
 
   def run(spark: SparkSession, config: ConfigType): Unit = {
-
     val conf = new Configuration
     conf.set("textinputformat.record.delimiter", "\n\n")
 
@@ -47,9 +56,7 @@ object StoreToElasticSearch extends Job {
         "text" -> sentence.text,
         "deps" -> sentence.deps
             .map{d => s"${d._2.deprel} >>> ${d._2.lemma} <<< ${sentence.deps(d._2.head).lemma}"}
-        , "deps_raw" -> sentence.deps.map(_._2).toList.sortBy(_.id).mkString("\n")
-      )
-
+        , "deps_raw" -> sentence.deps.map(_._2).toList.sortBy(_.id).mkString("\n"))
       }
     .saveToEs(config.outputIndex)
   }
@@ -64,8 +71,8 @@ object StoreToElasticSearch extends Job {
       .config("es.http.retries", "50")
       .config("es.batch.write.retry.count", "50")
       .config("es.batch.write.retry.wait", "300")
-      .config("es.batch.size.bytes","32000000")
-      .config("es.batch.size.entries","32000")
+      .config("es.batch.size.bytes", config.maxBatchMb.toString)
+      .config("es.batch.size.entries", config.maxBatchDocs.toString)
       //.config (read login and password of ES cluster here
       .getOrCreate()
 
@@ -75,7 +82,7 @@ object StoreToElasticSearch extends Job {
 
 /* Mapping:
 
-PUT depcc
+PUT test_default
 {
   "mappings": {
     "sentences": {
@@ -91,6 +98,7 @@ PUT depcc
         },
         "text": {
           "type": "text",
+          "analyzer": "english",
           "fields": {
             "keyword": {
               "ignore_above": 256,
@@ -111,20 +119,22 @@ PUT depcc
           "type": "text"
         },
         "deps_raw": {
-          "fields": {
-            "keyword": {
-              "type": "keyword",
-              "ignore_above": 256
-            }
-          },
-          "type": "text"
+          "type": "text",
+          "index": false
         }
       }
     }
   },
   "settings": {
+    "analysis": {
+      "analyzer": {
+        "my_english_analyzer":{
+          "type": "english"
+        }
+      }
+    },
     "index": {
-      "number_of_shards": "16",
+      "number_of_shards": "64",
       "number_of_replicas": "1"
     }
   }
