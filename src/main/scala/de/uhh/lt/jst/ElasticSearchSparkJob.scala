b3c7e35e68690e930fe88a5f0d403dbd1f2a1446
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-12-31 10:45:03 +0100

    Set Elastic Search config defaults in Parser

--- src/main/scala/de/uhh/lt/jst/ElasticSearchSparkJob.scala
+++ src/main/scala/de/uhh/lt/jst/ElasticSearchSparkJob.scala
@@ -8,32 +8,38 @@ abstract class ElasticSearchSparkJob extends SparkJob {
 
 
   abstract class ElasticSearchParser extends Parser {
+
+    val defaultUser = "elasticsearch"
     opt[String]("es-user").
       action { (x, c) => additionalSparkConf.set("spark.es.net.http.auth.user", x); c}.
       required().
-      text("User for Elastic Search, default: elasticsearch").
-      withFallback(() => "elasticsearch")
+      text(s"User for Elastic Search, default: $defaultUser").
+      withFallback(() => defaultUser)
 
     opt[String]("es-pass").
       action { (x, c) => additionalSparkConf.set("spark.es.net.http.auth.pass", x); c}.
       text("Password for Elastic Search").
       required()
 
-    // FIXME defaults is currently defined in conf/defaults.conf, good idea?
+    val defaultNode = "localhost"
     opt[String]("es-nodes").
       action { (x, c) => additionalSparkConf.set("spark.es.nodes", x); c}.
-      text("List of Elastic Search nodes, default: localhost")
+      text(s"List of Elastic Search nodes, default: $defaultNode").
+      withFallback(() => defaultNode)
 
-    // FIXME defaults is currently defined in conf/defaults.conf, good idea?
+    val defaultSize = 1000000
+    val bytesPerMB = 1000000
     opt[Int]("es-batch-size").
-      action { (x, c) => additionalSparkConf.set("spark.es.batch.size.bytes", s"${x * 1000000}"); c}.
+      action { (x, c) => additionalSparkConf.set("spark.es.batch.size.bytes", s"${x * bytesPerMB}"); c}.
       valueName("<int>").
-      text("Max. size of a batch in MB, default: 1")
+      text(s"Max. size of a batch in MB, default: ${defaultSize / bytesPerMB}").
+      withFallback(() => defaultSize)
 
-    // FIXME defaults is currently defined in conf/defaults.conf, good idea?
+    val defaultEntries = 1000
     opt[Int]("es-batch-entries").
       action { (x, c) => additionalSparkConf.set("es.batch.size.entries", s"$x"); c}.
       valueName("<int>").
-      text("Max. size of a batch in number of documents, default: 1000")
+      text(s"Max. size of a batch in number of documents, default: $defaultEntries").
+      withFallback(() => defaultEntries)
   }
 }
